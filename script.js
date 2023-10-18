ccc87372e1e56b185f3e7e7313040ad0033b233b
Matten Karma <Matten.karma@tptlive.ee>
2023-10-18 12:43:31 +0300

    Tehe

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-let array = [1, 2, true, 1.2, 'hello'];
-console.log(array);
-console.log(array[4]);
+let i = 3;
 
-array[3] = 69;
-console.log(array);
-array.push('new value');
-console.log(array);
-array = [
-    [1, 2, 3,],
-    [4, 5, 6,],
-    [7, 8, 9,]
-];
-console.log(array);
-console.log(array [1][2]);
-array = [[[[[1]]]]]
-console.log(array);
-console.log(array[0][0][0][0][0]);
+while(i<1_000){
+    console.log(i);
+    i*=5;
+}
 
-let object = {
-   name:'Matten',
-   age: 17,
-   cats: ['Brita', 'Mompsik']
-};
-console.log(object);
-console.log(object.name);
+
+
