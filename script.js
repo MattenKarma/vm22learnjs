0ab210bb99ccf2a8ba65b8efbe620cd4553db1e3
Matten Karma <Matten.karma@tptlive.ee>
2023-12-06 13:58:48 +0200

    Add form examples

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ nunjucks.configure('views', {
 });
 
 app.get('/', function (req, res) {
-  let name = 'Matten Karma' 
-  res.render('index.njk', {name});
+  let name = req.query.name; 
+  let age = req.query.age; 
+  res.render('index.njk', {name, age});
+});
+
+app.use(express.urlencoded());
+
+app.post('/answer', function (req, res) {
+  let name = req.body.name; 
+  let age = req.body.age; 
+  res.render('answer.njk', {name, age});
 });
 
 app.get('/about', function (req, res) {
